900fb3bbdd7d8fdee255ac83abdcb9a29dd47770
JonasTan <JonasTan2015@hotmail.com>
2016-12-27 12:20:48 -0500

    First commit. A skeleton of Node server. To do: complete user authentification methods

---
+++ db/MongoAPI.js
@@ -0,0 +1,26 @@
+const {mongoose} = require('./mongoose');
+const validator = require('validator');
+const _ = require('lodash');
+
+var UserSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 1,
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+      message: '{VALUE} is not a valid email'
+    }
+  },
+  password: {
+    type: String,
+    require: true,
+    minlength: 6
+  }
+});
+
+var User = mongoose.model('User', UserSchema);
+
+module.exports = {User};
---
+++ server.js
@@ -0,0 +1,76 @@
+require('./config.js');
+
+const mongo = require('./db/mongoose')
+const mongoose = require('./db/mongoose');
+const bodyParser = require('body-parser');
+const express = require('express');
+var session = require('express-session');
+var MongoDBStore = require('connect-mongodb-session')(session);
+const _ = require('lodash');
+const {User} = require ('./db/MongoAPI');
+const cookieParser = require('cookie-parser');
+
+var app = express();
+app.use(bodyParser.json());
+app.use(cookieParser());
+const port = process.env.PORT;
+
+
+var router = express.Router();
+
+// middleware that is specific to this router
+router.use(function timeLog(req, res, next) {
+  console.log('Time: ', Date.now());
+  next();
+});
+// define the home page route
+router.get('/', function(req, res) {
+  res.send('Birds home page');
+});
+
+router.post('/Login',function(req,res){
+  var body = _.pick(req.body,['email','password']);
+  User.findOne(body).then(function(doc){
+    if(!doc) return res.send({errMsg:"Invalid username or password"});
+    console.log(JSON.stringify(doc));
+    //res.cookie('sessionID', 'abcdefg').send('Cookie is set');
+
+    res.cookie('isVisit', 1, {maxAge: 60 * 1000});
+    res.send("欢迎第一次访问");
+  });
+
+});
+
+
+// define the about route
+router.post('/CreateUser',function(req,res){
+    var body = _.pick(req.body, ['email', 'password']);
+    if(!body.email||!body.password){
+      return res.send({errMsg:"Email and password cannot be blank"});
+    }
+    var tempuser=new User(body);
+
+    tempuser.save().then(function(doc){
+      console.log(doc);
+      res.send(req.body);
+    }).catch((e) => {
+    return res.send({errMsg:"Email already exists"});
+    });
+});
+
+router.get('/about', function(req, res) {
+  res.cookie('isVisit', '12345', {maxAge: 60 * 1000});
+    res.send("欢迎第一次访问");
+});
+
+
+app.get('/',router);
+app.post('/',router);
+app.post('/Login',router);
+app.get('/about',router);
+
+
+
+app.listen(port, () => {
+  console.log(`Started up at port ${port}`);
+});
